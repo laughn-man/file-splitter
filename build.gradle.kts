418e922dd8944b161f91bd4ac701c3ccae4652c4
Justin Carpenter <jbcarpenter1@gmail.com>
2021-10-17 08:33:11 -0400

    Adding shadowjar and configurable version number.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
 	kotlin("jvm") version "1.5.31"
+	id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
 group = "org.laughnman"
-version = "1.0-SNAPSHOT"
+version = "0.1.${ if (project.hasProperty("buildNumber")) project.property("buildNumber") else "0"}"
 
 val koinVersion = "3.1.2"
 
@@ -17,5 +21,20 @@ dependencies {
 	implementation("io.insert-koin:koin-core:$koinVersion")
 	implementation("info.picocli:picocli:4.6.1")
 	implementation("ch.qos.logback:logback-classic:1.2.6")
+}
 
+tasks {
+	named<ShadowJar>("shadowJar") {
+		archiveClassifier.set("")
+		mergeServiceFiles()
+		manifest {
+			attributes(mapOf("Main-Class" to "org.laughnman.filesplitter.ApplicationKt"))
+		}
+	}
+}
+
+tasks {
+	build {
+		dependsOn(shadowJar)
+	}
 }
