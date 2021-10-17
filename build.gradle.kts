b634bef09e03d9add8b11f0b2904769c635b2db3
Justin Carpenter <jbcarpenter1@gmail.com>
2021-10-17 09:35:06 -0400

    Fixing issue with target compatability.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "org.laughnman"
-version = "0.1.${ if (project.hasProperty("buildNumber")) project.property("buildNumber") else "0"}"
+version = "0.1.1"
 
 val koinVersion = "3.1.2"
 
@@ -21,6 +21,11 @@ dependencies {
 	implementation("io.insert-koin:koin-core:$koinVersion")
 	implementation("info.picocli:picocli:4.6.1")
 	implementation("ch.qos.logback:logback-classic:1.2.6")
+}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_1_8
+	targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks {
